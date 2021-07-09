b14275b74ef6a1e4fb6d2e559d33dc6309df6b7a
Lee Kiwan <leekiwan@Lees-MacBook-Air.local>
2021-07-09 11:01:17 +0900

    표지 page styledComponetn 적용

--- src/App.js
+++ src/App.js
@@ -3,32 +3,40 @@ import {DatePicker, Layout} from 'antd';
 import { Button, Tooltip } from "antd";
 import { PlayCircleOutlined } from "@ant-design/icons"
 // import { withRouter } from 'react-router-dom';
-
+import styled from 'styled-components';
 
 
 function App({ history }) {
   
   return (
       <Layout className="layout">
-          <Layout.Header className="header"><h1>Hello world</h1></Layout.Header>
+          <Layout.Header className="header"><h1>AssignMent Cover</h1></Layout.Header>
          
           <Layout.Content className="content">
               <p>
-                  <DatePicker />
+                 과제 작업날짜 :<StyledDatePicker />
               </p>
-              <span>가자 로그인페이지로!</span>
+              <span style={{fontSize:'15px' , fontWeight:'bold'}}>가자 로그인페이지로!</span>
               <Tooltip title="버튼">
-                <Button shape="round" onClick={(e)=>history.push('/main')} icon={<PlayCircleOutlined />} />
+                <StyledButton shape="round" onClick={(e)=>history.push('/main')} icon={<PlayCircleOutlined />} />
             </Tooltip>  
           
           </Layout.Content>
           <div className="Menu">
              
         </div>
-          <Layout.Footer className="footer">footer</Layout.Footer>
+          <Layout.Footer className="footer"></Layout.Footer>
       </Layout>
   );
 }
+const StyledDatePicker =styled(DatePicker)`
+margin-left: 10px;
+`;
+
+const StyledButton = styled(Button)`
+color :blue;
+margin-left: 10px;
+`;
 
 
 export default App;
